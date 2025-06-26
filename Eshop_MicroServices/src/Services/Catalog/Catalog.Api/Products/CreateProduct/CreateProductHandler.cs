cea2d936c28b0cedeabcdcbc7ddfaa3bc4f72140
baabak aghaei <baabakaghaei@gmail.com>
2025-06-27 00:18:25 +0330

    adding building block interfaces

--- Eshop_MicroServices/src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
+++ Eshop_MicroServices/src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,32 @@
-﻿namespace Catalog.Api.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+using Catalog.Api.Models;
+
+namespace Catalog.Api.Products.CreateProduct
 {
-    public class CreateProductHandler
+    public record CreateProductCommand(
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price) : ICommand<CreateProductResponse>;
+
+    public record CreateProductResponse(Guid Id);
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResponse> 
     {
+        public async Task<CreateProductResponse> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product()
+            {
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Name = command.Name,
+                Price = command.Price,
+            };
+
+            return new  CreateProductResponse( Guid.NewGuid());
+          
+        }
     }
 }
